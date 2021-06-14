5e98dc4b48067b8dae49c91e850de3081db369e4
sudeep <2018379@iiitdmj.ac.in>
2021-06-14 13:10:16 +0530

    second commit

--- src/router/booking.js
+++ src/router/booking.js
@@ -4,8 +4,12 @@ const db = require('../db/db')
 const bcrypt = require('bcryptjs')
 const dateFormat = require('../utils/shared')
 
-router.post('/hotel/:city', async (req, res) => {
+router.post('/hotels/:city', async (req, res) => {
     try {
+        req.body.dateOut = new Date(req.body.dateIn)
+        req.body.dateOut.setDate(req.body.dateOut.getDate() + req.body.stayDay)
+        req.body.dateOut = req.body.dateOut.toDateString("yyyy-MM-dd")
+        req.body.dateOut = dateFormat(req.body.dateOut)
         //console.log(req.body)
         const query1 = `(SELECT hotel.id as id,hotelName,Address,city,pinCode,state ,qtyRooms, rentPerRoomPerDay FROM rooms, hotel WHERE rooms.hotelId=hotel.id and hotel.city="${req.params.city}") as s1`
         //console.log(query1)
@@ -26,9 +30,12 @@ router.post('/hotel/:city', async (req, res) => {
     }
 })
 
-router.post('/booking', async (req, res) => {
+router.post('/hotel/:id', async (req, res) => {
     try {
+        console.log(req.body)
         const booking = req.body
+        console.log(req.params.id)
+        booking.hotelId = parseInt(req.params.id)
         booking.entryDate = Date.now()
         booking.entryDate = dateFormat(booking.entryDate)
         booking.dateOut = new Date(booking.dateIn)
@@ -39,18 +46,53 @@ router.post('/booking', async (req, res) => {
         const dateCondition = `(("${booking.dateOut}" > booking.dateIn and   "${booking.dateOut}" <= booking.dateOut)  or ( "${booking.dateIn}" >= booking.dateIn  and "${req.body.dateIn}" < booking.dateOut)) or ("${req.body.dateIn}" <= booking.dateIn and "${req.body.dateOut}" >= booking.dateOut)`
         const q1 = `SELECT sum(qtyRooms)  as booked FROM booking Where hotelId=${booking.hotelId} and ${dateCondition}`
         const q2 = `SELECT qtyRooms, rentPerRoomPerDay FROM rooms WHERE hotelId=${booking.hotelId}`
-        console.log(q1, q2)
         const resultQ1 = await db.promise().query(q1)
         const resultQ2 = await db.promise().query(q2);
-        //console.log(resultQ1)
-        if (resultQ1[0][0].booked + booking.qtyRooms > resultQ2[0][0].qtyRooms) {
+        console.log(resultQ2[0][0])
+        if (resultQ2[0][0].qtyRooms === undefined) {
+            return res.status(404).send("Hotel Not Fount")
+        }
+        const response = {}
+        response.availableRooms = resultQ2[0][0].qtyRooms - resultQ1[0][0].booked
+        const q3 = `SELECT * FROM hotel WHERE id=${booking.hotelId}`
+        const resultQ3 = await db.promise().query(q3)
+        response.hotel = resultQ3[0][0]
+        response.rent = resultQ2[0][0].rentPerRoomPerDay * booking.stayDay
+        res.status(200).send(response)
+    } catch (err) {
+        res.status(500).send("Server Error")
+    }
+})
+
+router.post('/booking', async (req, res) => {
+    try {
+        const booking = req.body
+        console.log(req.body)
+        booking.entryDate = Date.now()
+        booking.entryDate = dateFormat(booking.entryDate)
+        booking.dateOut = new Date(booking.dateIn)
+        booking.dateOut.setDate(booking.dateOut.getDate() + booking.stayDay)
+        booking.dateOut = booking.dateOut.toDateString("yyyy-MM-dd")
+        booking.dateOut = dateFormat(booking.dateOut)
+        console.log(booking)
+        const dateCondition = `(("${booking.dateOut}" > booking.dateIn and   "${booking.dateOut}" <= booking.dateOut)  or ( "${booking.dateIn}" >= booking.dateIn  and "${req.body.dateIn}" < booking.dateOut) or ("${req.body.dateIn}" <= booking.dateIn and "${req.body.dateOut}" >= booking.dateOut))`
+        const q1 = `SELECT sum(qtyRooms)  as booked FROM booking Where hotelId=${booking.hotelId} and ${dateCondition}`
+        const q2 = `SELECT qtyRooms, rentPerRoomPerDay FROM rooms WHERE hotelId=${booking.hotelId}`
+        console.log(q1)
+        const resultQ1 = await db.promise().query(q1)
+        const resultQ2 = await db.promise().query(q2);
+        console.log(parseInt(resultQ1[0][0].booked) + parseInt(booking.qtyRooms))
+        console.log(resultQ2[0][0].qtyRooms)
+        if (parseInt(resultQ1[0][0].booked) + parseInt(booking.qtyRooms) > parseInt(resultQ2[0][0].qtyRooms)) {
             return res.status(400).send("Suffecient room is not available for booking")
         }
         booking.rent = booking.qtyRooms * resultQ2[0][0].rentPerRoomPerDay * booking.stayDay
-        delete booking.stayDay
         const query = "INSERT INTO booking SET ?"
+        console.log(booking)
+        delete booking.stayDay
         db.query(query, booking, (err, result) => {
             if (err) {
+                console.log(err)
                 return res.status(400).send(err)
             }
             res.status(200).send(result)
